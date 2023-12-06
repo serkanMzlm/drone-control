5212269ac01ac8547addcc77f85e3f8864c20b1e
serkan <serkanmazlum306@gmail.com>
2023-12-06 08:15:20 +0300

    The package contents were divided into two parts     - The communication part with PX4 has been separated, and the     sections for control have been isolated.

--- controller/src/controller_node.cpp
+++ controller/src/controller_node.cpp
@@ -1,9 +1,5 @@
 #include "controller_node.hpp"
 
-#define COLOR_RED "\x1b[31m"
-#define COLOR_YLW "\x1b[33m"
-#define COLOR_GRN "\x1b[32m" 
-
 using namespace std::placeholders;
 using namespace px4_msgs::msg;
 
@@ -39,11 +35,6 @@ void Controller::detectFallCallback(){
 	}
 }
 
-void Controller::vehicleStatusCallback(const VehicleStatusMsg::UniquePtr msg){
-        // RCLCPP_INFO(this->get_logger(), "Arm Status: %d", msg->arming_state);
-		vehicle_arm_status = msg->arming_state;
-}
-
 void Controller::joyCallback(joyMsg msg){
 	joy_data.button[B_ARM] = msg.buttons[0];
 	joy_data.button[B_DISARM] = msg.buttons[1];
@@ -56,21 +47,6 @@ void Controller::joyCallback(joyMsg msg){
 	joy_data.axes[A_PITCH] = FILTER_OFFSET(msg.axes[4]);
 }
 
-void Controller::localPosCallback(localPosMsg::UniquePtr msg){
-	RCLCPP_DEBUG(this->get_logger(), "state: x: %.2f | y: %.2f | z: %.2f | yaw: %.2f",
-				msg->x, msg->y, msg->z, msg->heading);	
-	drone_state.position.x = msg->x;
-	drone_state.position.y = msg->y;
-	drone_state.position.z = msg->z;
-	drone_state.attitude.yaw = msg->heading;
-    fall_vel = msg->vz;
-	if(flag_first_point){
-		start_point = msg->z;
-		flag_first_point = false;
-	}
-}
-
-
 void Controller::controllerCallback(){
 	iniAirMode();
 	if(!getArming()){ return; }
@@ -78,51 +54,6 @@ void Controller::controllerCallback(){
 	controlMode(M_ATTITUDE);
 	vehicleCommand(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
 	trajectorySetpoint();
-}
-
-//////////////////////// PX4 Communication ////////////////////////
-void Controller::controlMode(Mode_e mod){
-	offboardControlModeMsg msg{};
-	msg.position     = mod == M_POSITION ? true : false;
-	msg.velocity     = mod == M_VELOCITY ? true : false;
-	msg.acceleration = mod == M_ACCELERATION ? true : false;
-	msg.attitude     = mod == M_ATTITUDE ? true : false;
-	msg.body_rate    = mod == M_BODY_RATE ? true : false;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-	pub.mode->publish(msg);
-}
-
-void Controller::trajectorySetpoint(){
- 	trajectorySetpointMsg msg{};
-	msg.position = {setpoint.position.x, setpoint.position.y, -setpoint.position.z};
-	msg.attitude = {setpoint.attitude.roll, setpoint.attitude.pitch, setpoint.attitude.thrust};
-	// msg.velocity = {setpoint.velocity.x, setpoint.velocity.y, -setpoint.velocity.z};
-	msg.yaw = setpoint.attitude.yaw;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-	pub.setpoint->publish(msg);
-}
-
-void Controller::fallTrajectorySetpoint(float error){
- 	trajectorySetpointMsg msg{};
-	msg.position = {setpoint.position.x, setpoint.position.y, error};
-	// msg.velocity = {setpoint.velocity.x, setpoint.velocity.y, -setpoint.velocity.z};
-	msg.yaw = setpoint.attitude.yaw;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-	pub.setpoint->publish(msg);
-}
-
-void Controller::vehicleCommand(uint16_t command, float param1, float param2){
-  	vehicleCommandMsg msg{};
-	msg.param1 = param1;
-	msg.param2 = param2;
-	msg.command = command;
-	msg.target_system = 1;
-	msg.target_component = 1;
-	msg.source_system = 1;
-	msg.source_component = 1;
-	msg.from_external = true;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-	pub.vehicle_command->publish(msg);
 }
 
 void Controller::initTopic(){
@@ -139,8 +70,6 @@ void Controller::initTopic(){
 							std::bind(&Controller::localPosCallback, this, _1));
 	sub.vehcile_status = this->create_subscription<VehicleStatusMsg>("/fmu/out/vehicle_status", qos,
                             std::bind(&Controller::vehicleStatusCallback, this, _1));
-	// sub.odom = this->create_subscription<odomMsg>("/fmu/out/vehicle_odometry", qos, 
-	// 						std::bind(&Controller::odomCallback, this, _1));
 
 	timer = this->create_wall_timer(std::chrono::milliseconds(F2P(50)), 
 							std::bind(&Controller::controllerCallback, this));
